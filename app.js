03b238ce512d2653e43f172de9b1befd33fb9f32
nadirbashir <1.nadirbashir@gmail.com>
2021-05-24 17:14:34 +0500

    comments removed

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
-// /*******Quest. 1 ********/
-// function pow(val,pwr){
-//     return val ** pwr;
-// }
-// var value = prompt("Enter number");
-// var power = prompt("Enter power value");
-// alert(value + " raised to " + power + " = " + pow(value,power) );
+/*******Quest. 1 ********/
+function pow(val,pwr){
+    return val ** pwr;
+}
+var value = prompt("Enter number");
+var power = prompt("Enter power value");
+alert(value + " raised to " + power + " = " + pow(value,power) );
 
-// /*******Quest. 2 ********/
-// function isLeapYear(year){
-//     if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
-//         return year + " is leap year";
-//     }else{
-//         return year + " is not leap year";
-//     }
-// }
-//  var year = prompt("Enter a year to check if its leap year");
-// alert(isLeapYear(year));
+/*******Quest. 2 ********/
+function isLeapYear(year){
+    if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+        return year + " is leap year";
+    }else{
+        return year + " is not leap year";
+    }
+}
+ var year = prompt("Enter a year to check if its leap year");
+alert(isLeapYear(year));
 
-// /*******Quest. 3 ********/
-// //method 1
-// function areaOfTriangle(a, b, c){
-//     function calcS(){
-//         return (a + b + c)/2; 
-//     }
-//     var s = calcS();
-//     return s*(s-a)*(s-b)*(s-c);
-// }
+/*******Quest. 3 ********/
+//method 1
+function areaOfTriangle(a, b, c){
+    function calcS(){
+        return (a + b + c)/2; 
+    }
+    var s = calcS();
+    return s*(s-a)*(s-b)*(s-c);
+}
 
-// alert("Area of triangle is " + areaOfTriangle(4,5,5))
+alert("Area of triangle is " + areaOfTriangle(4,5,5))
 
-// //method 2
-// function calcS(x, y, z){
-//     return (x + y + z)/2; 
-// }
-// function areaOfTriangle(a, b, c){
-//     var s = calcS(a, b, c);
-//     return s*(s-a)*(s-b)*(s-c);
-// }
+//method 2
+function calcS(x, y, z){
+    return (x + y + z)/2; 
+}
+function areaOfTriangle(a, b, c){
+    var s = calcS(a, b, c);
+    return s*(s-a)*(s-b)*(s-c);
+}
 
-// alert("Area of triangle is " + areaOfTriangle(4,5,5))
+alert("Area of triangle is " + areaOfTriangle(4,5,5))
 
-// /*******Quest. 4 ********/
+/*******Quest. 4 ********/
 
-// function average(sub1, sub2, sub3){
-//     return (sub1 + sub2 + sub3)/3;
-// }
-// function perc(sub1, sub2, sub3){
-//     return ((sub1 + sub2 + sub3)/300)*100;
-// }
-// function mainFunction(sub1, sub2, sub3){
-//     alert("Average = " + average(sub1,sub2,sub3).toFixed(2) + "\n Percentage = " + perc(sub1,sub2,sub3).toFixed(2));
-// }
-// var subject1 = +prompt("Enter marks obtained in subject 1 out of 100");
-// var subject2 = +prompt("Enter marks obtained in subject 1 out of 100");
-// var subject3 = +prompt("Enter marks obtained in subject 1 out of 100");
-// mainFunction(subject1, subject2, subject3);
+function average(sub1, sub2, sub3){
+    return (sub1 + sub2 + sub3)/3;
+}
+function perc(sub1, sub2, sub3){
+    return ((sub1 + sub2 + sub3)/300)*100;
+}
+function mainFunction(sub1, sub2, sub3){
+    alert("Average = " + average(sub1,sub2,sub3).toFixed(2) + "\n Percentage = " + perc(sub1,sub2,sub3).toFixed(2));
+}
+var subject1 = +prompt("Enter marks obtained in subject 1 out of 100");
+var subject2 = +prompt("Enter marks obtained in subject 1 out of 100");
+var subject3 = +prompt("Enter marks obtained in subject 1 out of 100");
+mainFunction(subject1, subject2, subject3);
 
-// /*******Quest. 5 ********/
+/*******Quest. 5 ********/
 
 
 
